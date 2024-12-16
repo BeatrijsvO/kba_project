91bae42b82b5962c2d180598a10464a78a89fd16
BeatrijsvO <beatrijs.van.oostveen@gmail.com>
2024-12-17 00:48:50 +0100

    Add CORS support

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Sta verzoeken toe vanaf andere domeinen
 
 @app.route("/kba", methods=["POST"])
 def answer_question():
