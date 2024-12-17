10b8aaf829dd462bea42dadd476d66dcf95236ea
BeatrijsvO <beatrijs.van.oostveen@gmail.com>
2024-12-17 14:24:49 +0100

    wijziging app

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ if __name__ == "__main__":
     import os
     print("Running production server with Waitress...")
     #serve(app, host="0.0.0.0", port=5000)
-    port = int(os.environ.get("PORT", 5000))  # Dynamische poort ophalen
+    port = int(os.environ.get("PORT", 4000))  # Dynamische poort ophalen
     serve(app, host="0.0.0.0", port=port)
