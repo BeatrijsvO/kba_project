2fff2192a5ec89fca3fbbe73088132dc30efc2b8
BeatrijsvO <beatrijs.van.oostveen@gmail.com>
2024-12-21 03:02:18 +0100

    kba

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://yininit.nl"}})
+CORS(app, resources={r"/kba": {"origins": "https://yininit.nl"}})
 
 @app.route("/kba", methods=["POST"])
 def answer_question():
