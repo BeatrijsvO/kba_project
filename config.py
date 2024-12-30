206ebb55182640d380685d07e83944895031e16b
BeatrijsvO <beatrijs.van.oostveen@gmail.com>
2024-12-30 02:59:43 +0100

    dir en alles

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 class Config:
-    """Configuratie voor de applicatie."""
-    APP_NAME = "KBA Webservice"
-    DEBUG = True
-    PORT = 10000  # Standaardpoort voor de applicatie
-    HOST = "0.0.0.0"  # Luisteren op alle netwerken
-    CORS_ORIGINS = ["https://yininit.nl"]  # Toegestane origins
+    CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*")  # Of specificeer je domein
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DEBUG = os.getenv("DEBUG", False)
