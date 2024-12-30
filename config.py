32ddf34eae01c194b8529b541355abbc07ce0610
BeatrijsvO <beatrijs.van.oostveen@gmail.com>
2024-12-30 13:47:46 +0100

    diverse

--- config.py
+++ config.py
@@ -3,4 +3,6 @@ import os
 class Config:
     CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*")  # Of specificeer je domein
     SECRET_KEY = os.getenv("SECRET_KEY")
+    SUPABASE_URL= os.getenv("SECRET_KEY")
+    SUPABASE_KEY = os.getenv("SECRET_KEY")
     DEBUG = os.getenv("DEBUG", False)
